19e4b763e32b473118069dd1ed7a86c5f32c3e13
Antonio Giannino <giannus@hotmail.it>
2016-05-11 10:52:20 +0200

    Fibonacci sequence

--- fibonacci.R
+++ fibonacci.R
@@ -2,16 +2,14 @@
 # by Antonio Giannino
 
 
-fibonacci <- function(n) {
-  x <- n
+fibonacci <- function(x) {
   fibonacci <- numeric(x)
   fibonacci[1] <- 0
   fibonacci[2] <- 1
   for (i in 3:x) { 
     fibonacci[i] <- fibonacci[i-1]+fibonacci[i-2]
   } 
-  fibonacci
+return(fibonacci)
 } 
-
 
 
