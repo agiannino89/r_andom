0ada5b5679ade724c029b226384e262307e1bd6a
Antonio Giannino <giannus@hotmail.it>
2016-05-26 23:34:42 +0200

    Compute the mode of several types of variables by group.

--- Mode.R
+++ Mode.R
@@ -1,3 +1,10 @@
-modes <- A %>%
+Mode <- function(x) {
+  ux <- unique(x)
+  ux[which.max(tabulate(match(x, ux)))]
+}
+
+# Example using dplyr
+
+modes <- A %>%      
   group_by(group_var) %>%
   summarise(mode = Mode(var))
